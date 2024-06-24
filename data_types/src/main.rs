85466e6382445ada0d84edbc6ebee4c021fe9359
Jackson Porter <jackson.porter49@gmail.com>
2024-06-24 12:28:17 -0500

    Comment added to data types main

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,6 +1,11 @@
-
-
-
 fn main() {
-    println!("Hello, world!");
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    /*
+    This is meant to be an extension of the guessing game program
+    Using the u32 represents an unsigned 32 bit integer, and the
+    program will check the answer given by the user. It then will
+    check that it is a 32 bit integer, and if not, will display the
+    error message "Not a number!"
+    */
 }
